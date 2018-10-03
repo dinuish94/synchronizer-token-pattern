d7a8945b4fcc49eb60787dffb5f08686ecbe29db
Dinuksha Ishwari <dinukshai@zone24x7.com>
2018-10-03 23:55:26 +0530

    Update application.properties location

--- src/main/java/com/sliit/ssd/csrfapp/constants/PropertyConstants.java
+++ src/main/java/com/sliit/ssd/csrfapp/constants/PropertyConstants.java
@@ -5,7 +5,7 @@ package com.sliit.ssd.csrfapp.constants;
  */
 public class PropertyConstants {
 
-    public static final String APP_PROPERTIES_FILE = "application.properties";
+    public static final String APP_PROPERTIES_FILE = "src/main/resources/application.properties";
 
     public static final String PROPERTY_USERNAME = "app.username";
     public static final String PROPERTY_PASSWORD = "app.password";
